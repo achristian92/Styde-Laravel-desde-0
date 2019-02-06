525187149136432a87c90b7c8fa10d504144b0f7
Alan Christian Ruiz Aguirre <christian922017@gmail.com>
2019-02-06 10:06:41 -0500

    insercion de datos con seeders

--- database/migrations/2019_10_12_000000_create_users_table.php
+++ database/migrations/2019_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->unsignedInteger('profession_id');
+            $table->unsignedInteger('profession_id')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,24 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        $this->TruncateTable([
+            'users',
+            'professions'
+        ]);
+
+
+        $this->call(ProfessionSeeder::class);
+        $this->call(UserSeeder::class);
+    }
+
+    public function TruncateTable(array $tables)
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0'); //Desactivar llaves foraneas
+
+        foreach ($tables as $table){
+            DB::table($table)->truncate(); //vaciar tabla
+        }
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1'); // volver activar llaves
     }
 }
