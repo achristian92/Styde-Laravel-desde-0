2aec71c3748681927737d14c7ba87a68a084491b
Alan Christian Ruiz Aguirre <christian922017@gmail.com>
2019-02-07 15:47:31 -0500

    agregando estilos boot 4 en listado usu

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,23 +1,43 @@
 @extends('layout')
 @section('content')
-    <h1>{{ $title }}  </h1>
-    <hr>
-    <h3><a href="{{route('users.create')}}">Nuevo Usu</a></h3>
+    <div class="d-flex justify-content-between align-items-end">
+        <h1 class="pb-1">{{ $title }}  </h1>
+        <p><a href="{{route('users.create')}}" class="btn btn-primary">Nuevo Usuario</a></h3></p>
+    </div>
+
     @if(count($users)) <!--condicional inverso(a menos que la lista usuario este vacia-->
-    <ul>
-        @foreach ($users as $user)
-            <li>{{ $user->name }}, ({{ $user->email }})
-            <a href="{{route('users.show',$user)}}">Ver +</a> |
-            <a href="{{route('users.edit',$user)}}">Edit</a> |
-                <form  action="{{route('users.destroy',$user)}}" method="post">
-                    {{ csrf_field() }}
-                    {{ method_field('delete') }}
-                    <button type="submit">Eliminar</button>
-                </form>
-        @endforeach
-    </ul>
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th>#</th>
+                <th>Nombre</th>
+                <th>correo</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+
+            @foreach ($users as $user)
+            <tr>
+                <th scope="row">{{ $user->id }}</th>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+                <td>
+                    <form  action="{{route('users.destroy',$user)}}" method="post">
+                        {{ csrf_field() }}
+                        {{ method_field('delete') }}
+                        <a href="{{route('users.show',$user)}}" class="btn btn-link"><span class="oi oi-eye"></span></a>
+                        <a href="{{route('users.edit',$user)}}" class="btn btn-link"><span class="oi oi-eyedropper"></span></a>
+                        <button type="submit" class="btn btn-link"><span class="oi oi-delete"></span></button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
     @else
         <p>No hay Usuarios Registrados</p>
     @endif
+
 
 @endsection
