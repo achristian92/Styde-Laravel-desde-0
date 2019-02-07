37b7176b177584cf3e6eaaa5b52c140193e982e9
Alan Christian Ruiz Aguirre <christian922017@gmail.com>
2019-02-07 15:13:19 -0500

    agregando enlaces mod usu

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,11 +1,19 @@
 @extends('layout')
 @section('content')
-    <h1>{{ $title }}</h1>
+    <h1>{{ $title }}  </h1>
     <hr>
+    <h3><a href="{{route('users.create')}}">Nuevo Usu</a></h3>
     @if(count($users)) <!--condicional inverso(a menos que la lista usuario este vacia-->
     <ul>
         @foreach ($users as $user)
-            <li>{{ $user->name }}, ({{ $user->email }}) <a href="{{route('users.show',$user->id)}}">Ver +</a> </li>
+            <li>{{ $user->name }}, ({{ $user->email }})
+            <a href="{{route('users.show',$user)}}">Ver +</a> |
+            <a href="{{route('users.edit',$user)}}">Edit</a> |
+                <form  action="{{route('users.destroy',$user)}}" method="post">
+                    {{ csrf_field() }}
+                    {{ method_field('delete') }}
+                    <button type="submit">Eliminar</button>
+                </form>
         @endforeach
     </ul>
     @else
