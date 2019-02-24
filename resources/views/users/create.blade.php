cf5642d375ac79681d1030ce27d802f9d4ca2e0a
Alan Christian Ruiz Aguirre <christian922017@gmail.com>
2019-02-24 11:29:14 -0500

    compartir html en vistas laravel componentes

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,25 +1,19 @@
 @extends('layout')
 @section('title',"Crear Usuario")
 @section('content')
-    <div class="card">
-        <div class="card-header">
-            <h4>Crear  Usuario</h4>
-        </div>
-        <div class="card-body">
+    @component('shared._card')
+        @slot('header')
+            Crear  Usuario
+        @endslot
+        @slot('content')
             @include('shared._errors')
             <form action="{{route('users.store')}}" method="post">
-
                 @include('users._fields')
-
-
                 <div class="form-group mt-4">
                     <button type="submit" class="btn btn-primary">Crear usuario</button>
                     <a href="{{route('users.index')}}" class="btn btn-link">Regresar</a>
                 </div>
-
-
-
             </form>
-        </div>
-    </div>
+        @endslot
+    @endcomponent
 @endsection
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,22 +1,24 @@
 @extends('layout')
 @section('title',"Editar Usuario")
 @section('content')
-    <div class="card">
-        <div class="card-header">
-            <h4>Editar Usuario # {{ $user->id }}</h4>
-        </div>
-        <div class="card-body">
-           @include('shared._errors')
+
+    @component('shared._card')
+        @slot('header')
+            Editar Usuario # {{ $user->id }}
+        @endslot
+        @slot('content')
+            @include('shared._errors')
 
             <form action="{{route('users.update',$user->id)}}" method="post">
                 {{ method_field('PUT') }}
 
                 @include('users._fields')
-            <div class="form-group mt-4">
-                <button type="submit" class="btn btn-primary">Actualizar Usuario</button>
-                <a href="{{route('users.index')}}" class="btn btn-link">Regresar</a>
-            </div>
+                <div class="form-group mt-4">
+                    <button type="submit" class="btn btn-primary">Actualizar Usuario</button>
+                    <a href="{{route('users.index')}}" class="btn btn-link">Regresar</a>
+                </div>
             </form>
-        </div>
-    </div>
+        @endslot
+    @endcomponent
+
 @endsection
